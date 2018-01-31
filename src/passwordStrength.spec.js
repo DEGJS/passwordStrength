20c88f3a397f908d5cf402fe46eb8d66943806a0
Joe Morgan <jmorgan@degdigital.com>
2018-01-31 12:58:29 -0600

    tests

---
+++ src/passwordStrength.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import passwordStrength from './passwordStrength';
+
+describe('Password Strength', () => {
+	it('should pass', () => {
+		document.body.innerHTML = `<div class="js-pwstrength"></div>`
+		const password = passwordStrength();
+		return expect(password.test('2%9ddiaA')).resolves.toEqual('passing');
+	});
+	it('should fail', async () => {
+		expect.assertions(1);
+		document.body.innerHTML = `<div class="js-pwstrength"></div>`
+		const password = passwordStrength();
+		try {
+			await password.test('foo');
+		} catch (e) {
+			expect(e).toEqual('failing')
+		}
+	});
+});
